f669fc1ec7e5cc5dc5cf8ac6dd5b2d80a9971011
ZOLMKOZ <ZOLMKOZ@ZOLMKOZ-PC>
2020-12-02 08:44:33 +0700

    Add project files.

---
+++ Text Filter/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace textfilter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] banWorlds = Console.ReadLine().Split(' ');
+            string text = Console.ReadLine();
+            foreach (var banWord in banWorlds)
+            {
+                if (text.Contains(banWord))
+                {
+                    text = text.Replace(banWord, "***");
+                }
+            }
+            Console.WriteLine(text);
+        }
+    }
+}
